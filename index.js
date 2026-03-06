8d2f372b2ae7209601ade51360de0aa7bda88a74
Tannu <tannu.sg06@gmail.com>
2026-03-06 13:49:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,5 @@ app.use('/api/products', productRoutes);
 app.get('/', (req, res) => {
   res.send('E-commerce catalog API is running');
 });
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
